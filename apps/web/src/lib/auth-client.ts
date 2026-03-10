336fa3ba06f6a379780f29a08b246399a67a96e3
Reynadi Andreas Priesley <rey@reynadi.com>
2026-03-10 22:43:20 +0700

    chore: remove fallback better auth public

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -3,6 +3,6 @@ import { inferAdditionalFields } from "better-auth/client/plugins";
 import type { auth } from "@sagentong/auth";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3001",
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
   plugins: [inferAdditionalFields<typeof auth>()],
 });
